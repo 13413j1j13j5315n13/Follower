262d9371052beafcfe9992dbb494750910778b40
qweqwe <qweqwe>
2020-09-09 01:11:04 +0300

    qwe

--- Follower.cs
+++ Follower.cs
@@ -21,59 +21,70 @@ namespace Follower
 {
     public class Follower : BaseSettingsPlugin<FollowerSettings>
     {
-        private readonly List<Entity> _entities = new List<Entity>();
-        private readonly Stopwatch _pickUpTimer = Stopwatch.StartNew();
         private readonly Stopwatch _debugTimer = Stopwatch.StartNew();
 
-        private Vector2 _clickWindowOffset;
         private WaitTime _workCoroutine;
         private uint _coroutineCounter;
         private bool _fullWork = true;
         private Coroutine _followerCoroutine;
 
-        private WaitTime ToPick => new WaitTime(5);
         private WaitTime Wait3ms => new WaitTime(3);
-        private WaitTime WaitForNextTry => new WaitTime(5);
 
-        private WaitTime WaitBeforeClicking => new WaitTime(5000);
+        private WaitTime Wait10ms => new WaitTime(10);
+
+        private Random _rand;
+        private DateTime _currentTime = DateTime.UtcNow;
+        private DateTime _lastTimeCastFlameDash;
 
         public override bool Initialise()
         {
+            LogMessage("****** Initialise started", 1);
+
+            _rand = new Random();
+            _lastTimeCastFlameDash = DateTime.UtcNow;
+
             _followerCoroutine = new Coroutine(MainWorkCoroutine(), this, "Follower");
             Core.ParallelRunner.Run(_followerCoroutine);
+
             _followerCoroutine.Pause();
             _debugTimer.Reset();
+
             Settings.MouseSpeed.OnValueChanged += (sender, f) => { Mouse.speedMouse = Settings.MouseSpeed.Value; };
             _workCoroutine = new WaitTime(Settings.ExtraDelay);
             Settings.ExtraDelay.OnValueChanged += (sender, i) => _workCoroutine = new WaitTime(i);
+
             return true;
         }
 
         private IEnumerator MainWorkCoroutine()
         {
+            LogMessage("****** MainWorkCoroutine started", 1);
             while (true)
             {
-                //yield return FindItemToPick();
-                yield return FollowPlayer();
+                yield return DoWork();
 
                 _coroutineCounter++;
                 _followerCoroutine.UpdateTicks(_coroutineCounter);
+
                 yield return _workCoroutine;
             }
         }
 
-
         public override Job Tick()
         {
+            _currentTime = DateTime.UtcNow;
+
             if (Input.GetKeyState(Keys.Escape)) _followerCoroutine.Pause();
 
-            if (Input.GetKeyState(Settings.PickUpKey.Value))
+            if (Input.GetKeyState(Settings.FollowerActivateKey.Value))
             {
                 _debugTimer.Restart();
 
                 if (_followerCoroutine.IsDone)
                 {
                     var firstOrDefault = Core.ParallelRunner.Coroutines.FirstOrDefault(x => x.OwnerName == nameof(Follower));
+
+                    LogMessage($"****** firstOrDefault: {firstOrDefault}", 1);
 
                     if (firstOrDefault != null)
                         _followerCoroutine = firstOrDefault;
@@ -91,174 +102,112 @@ namespace Follower
                 }
             }
 
-            if (_debugTimer.ElapsedMilliseconds > 2000)
+            int errorElapsedSeconds = 1000 * 30;
+
+            if (_debugTimer.ElapsedMilliseconds > errorElapsedSeconds)
             {
                 _fullWork = true;
-                LogMessage("Error pick it stop after time limit 2000 ms", 1);
+                LogMessage("errorElapsedSeconds has been elapsed. Turning off ", 1);
                 _debugTimer.Reset();
             }
 
             return null;
         }
 
-        private IEnumerable FollowPlayer()
+        private IEnumerator DoWork()
         {
-            if (Settings.LeaderName.Value == "")
-                yield break;
+            //LogMessage(" :::::::: DoWork called", 1);
 
-            IEnumerable<Entity> players  = GameController.Entities.Where(x => x.Type == EntityType.Player);
-            Entity leaderPlayer = SelectLeaderPlayer(players);
+            if (Settings.LeaderName == null || Settings.LeaderName.Value == "")
+            {
+                //LogMessage(" :::::::: Settings.LeaderName is null or empty!", 1);
+                yield break;
+            }
+
+            IEnumerable<Entity> players = GameController.Entities.Where(x => x.Type == EntityType.Player);
+
+            string leaderName = Settings.LeaderName.Value;
+            Entity leaderPlayer = SelectLeaderPlayer(players, leaderName);
+
+            //LogMessage($" :::::::: leaderPlayer {leaderPlayer}", 1);
 
             if (leaderPlayer == null)
+            {
+                //LogMessage($" :::::::: No leader player found", 1);
                 yield break;
+            }
 
             yield return TryToClickOnLeader(leaderPlayer);
+            //yield break;
         }
 
-        private Entity SelectLeaderPlayer(IEnumerable<Entity> players)
+        private Entity SelectLeaderPlayer(IEnumerable<Entity> players, string leaderName)
         {
-            string leaderName = Settings.LeaderName.Value;
             return players.First(x => x.GetComponent<Player>().PlayerName == leaderName);
         }
 
         private IEnumerator TryToClickOnLeader(Entity leaderPlayer)
         {
-            //var testi = leaderPlayer.;
-            //var centerOfItemLabel = pickItItem.LabelOnGround.Label.GetClientRectCache.Center;
-            //var rectangleOfGameWindow = GameController.Window.GetWindowRectangleTimeCache;
-            //var oldMousePosition = Mouse.GetCursorPositionVector();
-            //_clickWindowOffset = rectangleOfGameWindow.TopLeft;
-            //rectangleOfGameWindow.Inflate(-55, -55);
-            //centerOfItemLabel.X += rectangleOfGameWindow.Left;
-            //centerOfItemLabel.Y += rectangleOfGameWindow.Top;
+            //LogMessage(" ;;;;;;;;; TryToClickOnLeader called", 1);
 
             var worldCoords = leaderPlayer.Pos;
             Camera camera = GameController.Game.IngameState.Camera;
-            var mobScreenCoords = camera.WorldToScreen(worldCoords);
+            var result = camera.WorldToScreen(worldCoords);
 
-            Mouse.MoveCursorToPosition(mobScreenCoords);
+            var windowRectangle = GameController.Window.GetWindowRectangleReal();
+            var windowSize = new Size2F(windowRectangle.Width / 2560, windowRectangle.Height / 1600);
+
+            float scaledWidth = 50 * windowSize.Width;
+            float scaledHeight = 10 * windowSize.Height;
+
+            RectangleF aaa = new RectangleF(result.X - scaledWidth / 2f, result.Y - scaledHeight / 2f, scaledWidth,
+                scaledHeight);
+
+            Vector2 finalPos = new Vector2(aaa.X, aaa.Y);
+
+            //LogMessage("Moving and clicking mouse.", 5, Color.Red);
+
+            if (CanCastFlameDash())
+            {
+                LogMessage("Casting flame dash", 1);
+                yield return CastFlameDash(finalPos);
+            }
+            else
+            {
+                LogMessage("Clicking on the leader", 1);
+                yield return ClickOnLeader(finalPos);
+            }
+        }
+
+        private bool CanCastFlameDash()
+        {
+            var currentMs = ((DateTimeOffset) _currentTime).ToUnixTimeMilliseconds();
+            var lastTimeMs = ((DateTimeOffset) _lastTimeCastFlameDash).ToUnixTimeMilliseconds();
+            var delta = currentMs - lastTimeMs;
+
+            return Settings.FlameDash && delta > Settings.FlameDashCooldownMilliseconds.Value;
+        }
+
+        private IEnumerator CastFlameDash(Vector2 pos)
+        {
+            Mouse.MoveCursorToPosition(pos);
+            yield return Wait3ms;
+            Mouse.MoveCursorToPosition(pos);
+            yield return Wait10ms;
+            yield return Input.KeyPress(Settings.FlameDashKey);
+
+            _lastTimeCastFlameDash = _currentTime;
 
             yield break;
         }
 
-
-
-
-
-        /**
-             *    OLD CODE FROM HERE TO BE DELETED
-             */
-
-            public bool DoWePickThis(CustomItem itemEntity)
+        private IEnumerator ClickOnLeader(Vector2 pos)
         {
-            if (!itemEntity.IsValid)
-                return false;
-
-            return true;
-        }
-
-        private IEnumerator FindItemToPick()
-        {
-            if (!Input.GetKeyState(Settings.PickUpKey.Value) || !GameController.Window.IsForeground()) yield break;
-            var window = GameController.Window.GetWindowRectangleTimeCache;
-            var rect = new RectangleF(0, 0, window.Width, window.Height);
-            var playerPos = GameController.Player.GridPos;
-
-            /*            var currentLabelsDebugging = GameController.Game.IngameState.IngameUi.ItemsOnGroundLabels.ToList();
-                        currentLabelsDebugging = currentLabelsDebugging.Where(x => x.Address != 0).ToList();
-                        currentLabelsDebugging = currentLabelsDebugging.Where(x => x.ItemOnGround?.Path != null).ToList();
-                        currentLabelsDebugging = currentLabelsDebugging.Where(x => x.IsVisible).ToList();
-                        currentLabelsDebugging = currentLabelsDebugging.Where(x => x.Label.GetClientRectCache.Center.PointInRectangle(rect)).ToList();
-                        currentLabelsDebugging = currentLabelsDebugging.Where(x => x.CanPickUp || x.MaxTimeForPickUp.TotalSeconds <= 0).ToList();*/
-
-            List<CustomItem> currentLabels;
-            currentLabels = GameController.Game.IngameState.IngameUi.ItemsOnGroundLabels
-                .Where(x => x.Address != 0 &&
-                            x.ItemOnGround?.Path != null &&
-                            x.IsVisible && x.Label.GetClientRectCache.Center.PointInRectangle(rect) &&
-                            (x.CanPickUp || x.MaxTimeForPickUp.TotalSeconds <= 0))
-                .Select(x => new CustomItem(x, GameController.Files, x.ItemOnGround.DistancePlayer))
-                .OrderBy(x => x.Distance).ToList();
-
-
-            GameController.Debug["PickIt"] = currentLabels;
-            var pickUpThisItem = currentLabels.FirstOrDefault(x => DoWePickThis(x) && x.Distance < Settings.PickupRange);
-            if (pickUpThisItem?.GroundItem != null) yield return TryToPickV2(pickUpThisItem);
-            _fullWork = true;
-        }
-
-        private IEnumerator TryToPickV2(CustomItem pickItItem)
-        {
-            if (!pickItItem.IsValid)
-            {
-                _fullWork = true;
-                LogMessage("PickItem is not valid.", 5, Color.Red);
-                yield break;
-            }
-
-            var centerOfItemLabel = pickItItem.LabelOnGround.Label.GetClientRectCache.Center;
-            var rectangleOfGameWindow = GameController.Window.GetWindowRectangleTimeCache;
-            var oldMousePosition = Mouse.GetCursorPositionVector();
-            _clickWindowOffset = rectangleOfGameWindow.TopLeft;
-            rectangleOfGameWindow.Inflate(-55, -55);
-            centerOfItemLabel.X += rectangleOfGameWindow.Left;
-            centerOfItemLabel.Y += rectangleOfGameWindow.Top;
-
-            if (!rectangleOfGameWindow.Intersects(new RectangleF(centerOfItemLabel.X, centerOfItemLabel.Y, 3, 3)))
-            {
-                _fullWork = true;
-                //LogMessage($"Label outside game window. Label: {centerOfItemLabel} Window: {rectangleOfGameWindow}", 5, Color.Red);
-                yield break;
-            }
-
-            var tryCount = 0;
-
-            while (!pickItItem.IsTargeted() && tryCount < 5)
-            {
-                var completeItemLabel = pickItItem.LabelOnGround?.Label;
-
-                if (completeItemLabel == null)
-                {
-                    if (tryCount > 0)
-                    {
-                        LogMessage("Probably item already picked.", 3);
-                        yield break;
-                    }
-
-                    LogError("Label for item not found.", 5);
-                    yield break;
-                }
-
-                /*while (GameController.Player.GetComponent<Actor>().isMoving)
-                {
-                    yield return waitPlayerMove;
-                }*/
-                var clientRect = completeItemLabel.GetClientRect();
-
-                var clientRectCenter = clientRect.Center;
-
-                var vector2 = clientRectCenter + _clickWindowOffset;
-
-                Mouse.MoveCursorToPosition(vector2);
-                yield return Wait3ms;
-                Mouse.MoveCursorToPosition(vector2);
-                yield return Wait3ms;
-                yield return Mouse.LeftClick();
-                yield return ToPick;
-                tryCount++;
-            }
-
-            if (pickItItem.IsTargeted())
-                Input.Click(MouseButtons.Left);
-
-            tryCount = 0;
-
-            while (GameController.Game.IngameState.IngameUi.ItemsOnGroundLabels.FirstOrDefault(
-                       x => x.Address == pickItItem.LabelOnGround.Address) != null && tryCount < 6)
-            {
-                tryCount++;
-                yield return WaitForNextTry;
-            }
+            Mouse.MoveCursorToPosition(pos);
+            yield return Wait3ms;
+            Mouse.MoveCursorToPosition(pos);
+            yield return Wait10ms;
+            yield return Mouse.LeftClick();
         }
     }
 }
--- FollowerSettings.cs
+++ FollowerSettings.cs
@@ -1,6 +1,7 @@
 using System.Windows.Forms;
 using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
+using ExileCore.Shared.Attributes;
 
 namespace Follower
 {
@@ -9,32 +10,45 @@ namespace Follower
         public FollowerSettings()
         {
             Enable = new ToggleNode(false);
-            PickUpKey = Keys.F1;
-            PickupRange = new RangeNode<int>(600, 1, 1000);
-            ChestRange = new RangeNode<int>(500, 1, 1000);
+            FollowerActivateKey = Keys.F2;
             ExtraDelay = new RangeNode<int>(0, 0, 200);
-            GroundChests = new ToggleNode(false);
-            PickUpEverything = new ToggleNode(false);
-            LeftClickToggleNode = new ToggleNode(true);
-            OverrideItemPickup = new ToggleNode(false);
             MouseSpeed = new RangeNode<float>(1, 0, 30);
             LeaderName = new TextNode("");
+
+            FlameDash = new ToggleNode(false);
+            FlameDashKey = new HotkeyNode(Keys.Q);
+            FlameDashCooldownMilliseconds = new RangeNode<int>(3500, 0, 10000);
+
+
+            //PickupRange = new RangeNode<int>(600, 1, 1000);
+            //ChestRange = new RangeNode<int>(500, 1, 1000);
+            //GroundChests = new ToggleNode(false);
+            //PickUpEverything = new ToggleNode(false);
+            //LeftClickToggleNode = new ToggleNode(true);
+            //OverrideItemPickup = new ToggleNode(false);
         }
 
         public TextNode LeaderName { get; set; }
-
         public ToggleNode Enable { get; set; }
-        public HotkeyNode PickUpKey { get; set; }
-        public RangeNode<int> PickupRange { get; set; }
-        public RangeNode<int> ChestRange { get; set; }
+        public HotkeyNode FollowerActivateKey { get; set; }
         public RangeNode<int> ExtraDelay { get; set; }
-        public EmptyNode AllOverridEmptyNode { get; set; }
-        public ToggleNode PickUpEverything { get; set; }
-        public ToggleNode GroundChests { get; set; }
-        public ToggleNode LeftClickToggleNode { get; set; }
-        public ToggleNode OverrideItemPickup { get; set; }
         public RangeNode<float> MouseSpeed { get; set; }
-        public ToggleNode ReturnMouseToBeforeClickPosition { get; set; } = new ToggleNode(true);
-        public RangeNode<int> TimeBeforeNewClick { get; set; } = new RangeNode<int>(500, 0, 1500);
+
+        [Menu("Flame Dash", 5)] public ToggleNode FlameDash { get; set; }
+
+        [Menu("Flame Dash Key", 51, 5)] public HotkeyNode FlameDashKey { get; set; }
+
+        [Menu("Flame Dash cooldown in milliseconds (default 3.5 seconds)", "", 52, 5)]
+        public RangeNode<int> FlameDashCooldownMilliseconds { get; set; }
+
+        //public RangeNode<int> PickupRange { get; set; }
+        //public RangeNode<int> ChestRange { get; set; }
+        //public EmptyNode AllOverridEmptyNode { get; set; }
+        //public ToggleNode PickUpEverything { get; set; }
+        //public ToggleNode GroundChests { get; set; }
+        //public ToggleNode LeftClickToggleNode { get; set; }
+        //public ToggleNode OverrideItemPickup { get; set; }
+        //public ToggleNode ReturnMouseToBeforeClickPosition { get; set; } = new ToggleNode(true);
+        //public RangeNode<int> TimeBeforeNewClick { get; set; } = new RangeNode<int>(500, 0, 1500);
     }
 }
